a149dffd28c90332285b5c0ec438ca74a28096cb
Razy292 <rahmanzaini292@gmail.com>
2026-05-12 09:27:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -443,4 +443,4 @@ def switch_model():
 load_original_model()
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
